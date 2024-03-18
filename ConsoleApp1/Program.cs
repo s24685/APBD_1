fb8d67e5fb13760ec307674df6db4cb3115b08aa
s24685 <s24685@pjwstk.edu.pl>
2024-03-18 17:32:56 +0100

    feature-average

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Modification 1");
-Console.WriteLine("Modification 2");
-Console.WriteLine("Modification 3");
+int[] numbers = { 10, 20, 30, 40, 50 };
+double average = AverageCalculator.CalculateAverage(numbers);
+Console.WriteLine("Average: " + average);
+
+public class AverageCalculator
+{
+    public static double CalculateAverage(int[] numbers)
+    {
+        if (numbers == null || numbers.Length == 0)
+            return 0; // Return 0 if the array is null or empty
+
+        int sum = 0;
+        foreach (int num in numbers)
+            sum += num;
+        return (double)sum / numbers.Length;
+    }
+}
+
+
